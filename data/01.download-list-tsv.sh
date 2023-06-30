d0fb2fd35929c88767de9996024fb2a54b0447d5
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-06-30 08:42:43 -0400

    add a script to download the list of bioproject

---
+++ data/01.download-list-tsv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -o XENLA.list.tsv -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8355)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
+curl -o XENTR.list.tsv -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8364)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"

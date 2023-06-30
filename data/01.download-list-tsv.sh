0cd2dd9b3d86d601694d8b7ce2ffb2554430d8b8
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-06-30 08:48:22 -0400

    add FASTQ FTP URL to the study list

--- data/01.download-list-tsv.sh
+++ data/01.download-list-tsv.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-curl -o XENLA.list.tsv -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8355)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
-curl -o XENTR.list.tsv -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8364)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
+DATE=$(date +"%Y-%m-%d")
+
+XENLA_LIST="XENLA_study_list."$DATE".tsv"
+XENTR_LIST="XENTR_study_list."$DATE".tsv"
+
+curl -o $XENLA_LIST -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8355)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cfastq_ftp%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
+curl -o $XENTR_LIST -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_study&query=tax_eq(8364)&fields=tax_id%2Cstudy_accession%2Cstudy_alias%2Csubmission_accession%2Clibrary_strategy%2Crun_accession%2Cfastq_ftp%2Cstudy_title&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
